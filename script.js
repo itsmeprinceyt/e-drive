994bb122ca9625d4cf76baff472cd67e5898f907
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-03-17 15:48:56 +0530

    Update

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ document.querySelector('.icon').addEventListener('click',() =>
 {
   myFunction();
 });
-document.getElementById('whatsappButton').addEventListener('click', function() {
-  window.location.href = 'https://wa.link/q52old';
-});
-document.getElementById('whatsappbroadcastbutton').addEventListener('click', function() {
-  window.location.href = 'https://whatsapp.com/channel/0029VaJwSV32ZjCuU1Fuue3u';
+document.getElementById('discord-server').addEventListener('click', function() {
+  window.location.href = 'https://discord.gg/AeQWRsrtqg';
 });
 function myFunction() {
     var x = document.getElementById("myLinks");
