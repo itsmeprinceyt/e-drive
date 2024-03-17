99eceb62cc6c6975e2161e2be88eca04add7eda8
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-03-17 15:00:03 +0530

    Update

--- contact.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('whatsappButton').addEventListener('click', function() {
-    window.location.href = 'https://wa.link/q52old';
-  });
-document.getElementById('whatsappbroadcastbutton').addEventListener('click', function() {
-    window.location.href = 'https://whatsapp.com/channel/0029VaJwSV32ZjCuU1Fuue3u';
-});
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.querySelector('.icon').addEventListener('click',() =>
+{
+  myFunction();
+});
+document.getElementById('whatsappButton').addEventListener('click', function() {
+  window.location.href = 'https://wa.link/q52old';
+});
+document.getElementById('whatsappbroadcastbutton').addEventListener('click', function() {
+  window.location.href = 'https://whatsapp.com/channel/0029VaJwSV32ZjCuU1Fuue3u';
+});
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -5,4 +15,4 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
-  }
+}
